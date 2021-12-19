d5d7af6543f58b2f77bc0af93c6831d44821ad71
Filip Drygaś <tejotemku@gmail.com>
2021-12-19 13:36:03 +0100

    password change component

--- services/frontend/src/views/admin/AdminPage.vue
+++ services/frontend/src/views/admin/AdminPage.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="max-screen-space-container flex-column-items-centered">
     <Navbar v-bind:usertype="'admin'" />
+    <ChangePassword />
   </div>
 </template>
 
 <script>
 import Navbar from '../../components/Navbar.vue'
+import ChangePassword from '../../components/ChangePassword.vue'
 
 export default {
   name: 'AdminPage',
   components: {
     Navbar,
+    ChangePassword,
   },
 }
 </script>
